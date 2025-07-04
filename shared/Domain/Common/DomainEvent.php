81f4f55fcee956be6f4638c5fe26f73a1e07fdec
Sergio <sergioalmela@gmail.com>
2025-07-04 22:00:51 +0200

    Create domain event

---
+++ shared/Domain/Common/DomainEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\Domain\Event;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+
+interface DomainEvent
+{
+    public function eventType(): string;
+    public function eventVersion(): int;
+    public function aggregateId(): string;
+    public function occurredOn(): DateTimeImmutable;
+    public function payload(): array;
+    public function toArray(): array;
+}
+
+abstract readonly class BaseDomainEvent implements DomainEvent
+{
+    public function __construct(
+        protected string $aggregateId,
+        protected DateTimeImmutable $occurredOn
+    ) {}
+
+    abstract public function eventType(): string;
+    abstract public function eventVersion(): int;
+    abstract public function payload(): array;
+
+    public function aggregateId(): string
+    {
+        return $this->aggregateId;
+    }
+
+    public function occurredOn(): DateTimeImmutable
+    {
+        return $this->occurredOn;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'eventType' => $this->eventType(),
+            'eventVersion' => $this->eventVersion(),
+            'aggregateId' => $this->aggregateId(),
+            'payload' => $this->payload(),
+            'occurredOn' => $this->occurredOn()->format(DateTimeInterface::ATOM),
+        ];
+    }
+}
