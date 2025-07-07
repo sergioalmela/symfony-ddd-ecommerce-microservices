8501939408a0f8a7263d591de63ec5731c420931
Sergio <sergioalmela@gmail.com>
2025-07-07 10:33:37 +0200

    Add swagger

--- src/Order/Infrastructure/Http/Controller/GetOrdersController.php
+++ src/Order/Infrastructure/Http/Controller/GetOrdersController.php
@@ -6,6 +6,7 @@ namespace App\Order\Infrastructure\Http\Controller;
 
 use App\Order\Application\Query\GetOrders\GetOrdersQuery;
 use App\Shared\Infrastructure\Http\Controller\BaseController;
+use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,6 +14,37 @@ use Symfony\Component\Routing\Attribute\Route;
 final class GetOrdersController extends BaseController
 {
     #[Route('/orders', methods: ['GET'])]
+    #[OA\Get(
+        summary: 'Get orders',
+        description: 'Retrieve a list of orders for a seller',
+        tags: ['Orders']
+    )]
+    #[OA\Parameter(
+        name: 'sellerId',
+        description: 'The seller ID to filter orders',
+        in: 'query',
+        required: false,
+        schema: new OA\Schema(type: 'string', example: '80ebe6dd-fc51-41d5-ad53-59386d3ee6aa')
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'List of orders',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(
+                type: 'object',
+                properties: [
+                    new OA\Property(property: 'id', type: 'string'),
+                    new OA\Property(property: 'productId', type: 'string'),
+                    new OA\Property(property: 'quantity', type: 'integer'),
+                    new OA\Property(property: 'price', type: 'number'),
+                    new OA\Property(property: 'customerId', type: 'string'),
+                    new OA\Property(property: 'sellerId', type: 'string'),
+                    new OA\Property(property: 'status', type: 'string'),
+                ]
+            )
+        )
+    )]
     public function getOrders(Request $request): JsonResponse
     {
         $query = new GetOrdersQuery(
