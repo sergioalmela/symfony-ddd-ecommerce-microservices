59b251958483de5073db45849837f980a29efdc4
Sergio <sergioalmela@gmail.com>
2025-07-08 12:37:38 +0200

    Fix get orders query handler

--- src/Order/Application/Query/GetOrders/GetOrdersQueryHandler.php
+++ src/Order/Application/Query/GetOrders/GetOrdersQueryHandler.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 namespace App\Order\Application\Query\GetOrders;
 
 use App\Order\Domain\Repository\OrderRepository;
+use App\Shared\Domain\Bus\Query\QueryHandler;
 use App\Shared\Domain\ValueObject\SellerId;
 
-final readonly class GetOrdersQueryHandler
+final readonly class GetOrdersQueryHandler implements QueryHandler
 {
     public function __construct(
         private OrderRepository $orderRepository
