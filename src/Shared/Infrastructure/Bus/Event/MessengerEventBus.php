a4f83097c695501084d890e48e9676eb853985c7
Sergio <sergioalmela@gmail.com>
2025-07-09 09:13:05 +0200

    Create shared bus

---
+++ src/Shared/Infrastructure/Bus/Event/MessengerEventBus.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Bus\Event;
+
+use App\Shared\Domain\Bus\Event\EventBus;
+use App\Shared\Domain\Event\DomainEvent;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+final class MessengerEventBus implements EventBus
+{
+    private MessageBusInterface $eventBus;
+
+    public function __construct(MessageBusInterface $eventBus)
+    {
+        $this->eventBus = $eventBus;
+    }
+
+    public function publish(DomainEvent ...$events): void
+    {
+        foreach ($events as $event) {
+            $this->eventBus->dispatch($event);
+        }
+    }
+}
